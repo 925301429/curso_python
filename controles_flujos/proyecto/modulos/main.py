53ceaec9f618d27f0e16341560cc9a61e9467727
lisbet_zevallos <marianazevallos9@gmail.com>
2024-07-15 10:19:22 -0500

    suvi la clases de 15 de julio

--- controles_flujos/proyecto/modulos/main.py
+++ controles_flujos/proyecto/modulos/main.py
@@ -5,10 +5,8 @@ import es_menor
 import cuenta_vocales
 funciones_mayor_edad(20)
 print(es_mayor . f_es_mayor([7,4,2,1,0,8]))
-print(es_menor.f_es menor([7_]))
-
-
-mayor_edad(20) 
+print(es_menor.f_es menor([7,4,5,2,3,0]))
+print(cuenta_vocales.f_cuenta_vocales("golacosyynh lrrork"))
 print(es_mayor([4,8,10,2,3])) 
 print(cuenta_vocales("mi mama me mima yo amo ami mama"))
 
